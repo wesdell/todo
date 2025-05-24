dd7edc62a7f9d89127a4b2313d55e6223bfcdae9
Wesdell <wesdell17@gmail.com>
2025-05-23 21:20:06 -0500

    Define User entity

---
+++ src/main/java/com/wesdell/todoapi/entities/User.java
@@ -0,0 +1,31 @@
+package com.wesdell.todoapi.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @SequenceGenerator(name = "article_sequence", sequenceName = "article_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "article_sequence")
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Task> tasks;
+}
