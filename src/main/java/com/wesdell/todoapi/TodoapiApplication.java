d4575985bd109af4b361f5943eeed77b0f9c214c
Wesdell <wesdell17@gmail.com>
2025-05-25 17:51:10 -0500

    Enable read application properties

--- src/main/java/com/wesdell/todoapi/TodoapiApplication.java
+++ src/main/java/com/wesdell/todoapi/TodoapiApplication.java
@@ -1,9 +1,12 @@
 package com.wesdell.todoapi;
 
+import com.wesdell.todoapi.utils.ApplicationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(ApplicationProperties.class)
 public class TodoapiApplication {
 
 	public static void main(String[] args) {
