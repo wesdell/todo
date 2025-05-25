5008844fcc16311893e8e1d21a8d27ba0fa4d11f
Wesdell <wesdell17@gmail.com>
2025-05-24 20:47:13 -0500

    Change types

--- src/main/java/com/wesdell/todoapi/interfaces/IUserService.java
+++ src/main/java/com/wesdell/todoapi/interfaces/IUserService.java
@@ -1,16 +1,15 @@
 package com.wesdell.todoapi.interfaces;
 
-import com.wesdell.todoapi.dto.CreateUserDto;
-import com.wesdell.todoapi.dto.LoginUserDto;
-import com.wesdell.todoapi.dto.UpdateUserDto;
 import com.wesdell.todoapi.entities.User;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface IUserService {
-    User register(CreateUserDto dto);
-    Optional<User> login(LoginUserDto dto);
-    User update(Long userId, UpdateUserDto dto);
+    List<User> getUsers();
+    User register(User user);
+    Optional<User> login(User userAttemptedToLog);
+    User update(Long userId, User updatedUser);
     Optional<User> findByEmail(String email);
     Optional<User> findById(Long id);
 }
