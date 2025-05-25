b00c5c57cd16c80707f073658fdbd04f5afd6f48
Wesdell <wesdell17@gmail.com>
2025-05-24 20:51:13 -0500

    Add User controller to send different rest operations

---
+++ src/main/java/com/wesdell/todoapi/controllers/UserController.java
@@ -0,0 +1,50 @@
+package com.wesdell.todoapi.controllers;
+
+import com.wesdell.todoapi.dto.CreateUserDto;
+import com.wesdell.todoapi.dto.UpdateUserDto;
+import com.wesdell.todoapi.dto.UserDto;
+import com.wesdell.todoapi.mappers.UserMapper;
+import com.wesdell.todoapi.services.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/users")
+public class UserController {
+    private final UserService userService;
+    private final UserMapper userMapper;
+
+    @Autowired
+    public UserController(UserService userService, UserMapper userMapper) {
+        this.userService = userService;
+        this.userMapper = userMapper;
+    }
+
+    @GetMapping()
+    public List<UserDto> getAllUsers() {
+        return userService.getUsers().stream().map(userMapper::toDto).toList();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable long id) {
+        return userService.findById(id)
+            .map(userMapper::toDto)
+            .map(ResponseEntity::ok)
+            .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping()
+    public UserDto createUser(@Valid @RequestBody CreateUserDto createUserDto) {
+        return userMapper.toDto(userService.register(userMapper.toEntity(createUserDto)));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDto> updateUser(@PathVariable long id, @Validated @RequestBody UpdateUserDto updateUserDto) {
+        return ResponseEntity.ok(userMapper.toDto(userService.update(id, userMapper.toEntity(updateUserDto))));
+    }
+}
