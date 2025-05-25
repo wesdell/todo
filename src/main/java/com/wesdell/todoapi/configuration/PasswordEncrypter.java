b7b633cdf2ef06f169b14be8f013fa84132554c7
Wesdell <wesdell17@gmail.com>
2025-05-25 13:06:04 -0500

    Update package name for better understanding

--- src/main/java/com/wesdell/todoapi/configuration/PasswordEncrypter.java
+++ src/main/java/com/wesdell/todoapi/configuration/PasswordEncrypter.java
@@ -1,4 +1,4 @@
-package com.wesdell.todoapi.utils;
+package com.wesdell.todoapi.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
