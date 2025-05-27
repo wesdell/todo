c4f726990b137c9bd340e3cf06b52a689eee239e
Wesdell <wesdell17@gmail.com>
2025-05-26 19:54:06 -0500

    New endpoints for auth controller to handle register and login requests

---
+++ src/main/java/com/wesdell/todoapi/controllers/AuthenticationController.java
@@ -0,0 +1,35 @@
+package com.wesdell.todoapi.controllers;
+
+
+import com.wesdell.todoapi.dto.AuthenticationDto;
+import com.wesdell.todoapi.dto.LoginUserDto;
+import com.wesdell.todoapi.dto.RegisterUserDto;
+import com.wesdell.todoapi.services.AuthenticationService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/auth")
+public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+
+    @Autowired
+    public AuthenticationController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationDto> register(@Valid @RequestBody RegisterUserDto registerUserDto) {
+        return ResponseEntity.ok(authenticationService.register(registerUserDto));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationDto> login(@Valid @RequestBody LoginUserDto dto) {
+        return ResponseEntity.ok(authenticationService.login(dto));
+    }
+}
