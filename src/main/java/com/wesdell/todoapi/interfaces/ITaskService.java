024f0157022d3475c15d370975f886a20f6c5f1a
Wesdell <wesdell17@gmail.com>
2025-05-30 21:00:32 -0500

    Update task service interface

--- src/main/java/com/wesdell/todoapi/interfaces/ITaskService.java
+++ src/main/java/com/wesdell/todoapi/interfaces/ITaskService.java
@@ -6,10 +6,9 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ITaskService {
-    List<Task> getTasks();
-    List<Task> getTasksByUserId(Long id);
-    Task createTask(Task task);
-    Task updateTask(Task task);
-    void deleteTask(Long id);
-    Optional<Task> findTaskById(Long id);
+    List<Task> getTasksByUserEmail(String email);
+    Task createTask(String userEmail, Task task);
+    Task updateTask(Long id, String userEmail, Task updatedTask);
+    void deleteTask(Long id, String userEmail);
+    Optional<Task> findTaskByIdAndUserEmail(Long id, String email);
 }
