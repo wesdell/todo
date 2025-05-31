e71e3e6b63d923d7277fe5a11802145adddeb493
Wesdell <wesdell17@gmail.com>
2025-05-30 20:58:19 -0500

    Modify task mapper to avoid set to null non send fields on update rest operation

--- src/main/java/com/wesdell/todoapi/mappers/TaskMapper.java
+++ src/main/java/com/wesdell/todoapi/mappers/TaskMapper.java
@@ -4,11 +4,15 @@ import com.wesdell.todoapi.dto.CreateTaskDto;
 import com.wesdell.todoapi.dto.TaskDto;
 import com.wesdell.todoapi.dto.UpdateTaskDto;
 import com.wesdell.todoapi.entities.Task;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = "spring")
 public interface TaskMapper {
     TaskDto toDto(Task task);
     Task toEntity(CreateTaskDto createTaskDto);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     Task toEntity(UpdateTaskDto updateTaskDto);
 }
