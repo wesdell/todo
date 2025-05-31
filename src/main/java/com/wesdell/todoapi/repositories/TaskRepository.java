c1097fbd03795ec8eadcee5d2f742b0d478da203
Wesdell <wesdell17@gmail.com>
2025-05-30 20:48:26 -0500

    New functionality to fetch a task by its id and user

--- src/main/java/com/wesdell/todoapi/repositories/TaskRepository.java
+++ src/main/java/com/wesdell/todoapi/repositories/TaskRepository.java
@@ -5,7 +5,9 @@ import com.wesdell.todoapi.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
     List<Task> findByUser(User user);
+    Optional<Task> findByIdAndUser(Long id, User user);
 }
