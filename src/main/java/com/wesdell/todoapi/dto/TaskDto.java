82c2c8841a209ef06e53a624c4ea8315d8b70460
Wesdell <wesdell17@gmail.com>
2025-06-01 20:13:04 -0500

    Add lombook all and no args constructor decorator

--- src/main/java/com/wesdell/todoapi/dto/TaskDto.java
+++ src/main/java/com/wesdell/todoapi/dto/TaskDto.java
@@ -1,12 +1,16 @@
 package com.wesdell.todoapi.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class TaskDto {
     private Long id;
     private String title;
